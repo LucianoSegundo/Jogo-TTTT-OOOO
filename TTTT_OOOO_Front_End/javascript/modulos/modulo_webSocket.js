a17677ef85a93e49cc7fef1728e49cc2817ccf2d
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-08-22 13:08:21 -0300

    feat: atualização

--- TTTT_OOOO_Front_End/javascript/modulos/modulo_webSocket.js
+++ TTTT_OOOO_Front_End/javascript/modulos/modulo_webSocket.js
@@ -9,7 +9,7 @@ function criarWebSocket(url) {
      socket = new WebSocket(url);
 
    } catch (error) {
-       let url ='wss://jogo-tttt-oooo-back-end.onrender.com';
+       let url ='ws://jogo-tttt-oooo-back-end.onrender.com';
        console.log(url)
        socket = new WebSocket(url);
    }
